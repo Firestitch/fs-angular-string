9f239b425263987f44d08f3c707cf27f8052b5d8
Raymond <ray@firestitch.com>
2016-12-15 13:55:08 -0500

    added filter docs

--- src/app/scripts/filters/filter.js
+++ src/app/scripts/filters/filter.js
@@ -1,6 +1,11 @@
 (function () {
     'use strict';
-
+	/**
+	 * @ngdoc filter
+	 * @name fs.filters:fsString
+	 * @param {integer} count The number of objects
+	 * @param {string} plural The word used when there are zero or multiple objects
+	 */
     angular.module('fs-angular-string',[])
      .filter('fsStringPlural', function(fsString) {
 		return function(singular,count,plural) {
